5af36615f2e508e548e7c9e56b9c52b21e8f68e2
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-14 22:07:29 +0530

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,23 @@
+
+class Lab1 {
+    public static void main(String args[])
+ {
+
+        String s1 = "Hello guys";
+        String s2 = "123 guys";
+        String s3 = " ";
+        
+	int a = 10;
+        int b = 20;
+
+
+        System.out.println(-a);
+        System.out.println(b);
+    }
+}
+
+/*
+O/P:
+-10
+20
+*/
