640f481a4cf09021f606bb8f86c98a4e290cca62
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-14 22:16:00 +0530

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,37 @@
+class Lab2
+{
+
+byte b;
+short s;
+int i;
+long l;
+float f;
+double d;
+
+void show1()
+{
+System.out.println("variable & constant demo");
+System.out.println(b);
+System.out.println(s);
+System.out.println(i);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+}
+public static void main(String args[])
+{
+Lab2 l2=new Lab2();
+l2.show1();
+}
+}
+
+/*
+O/P:
+0
+0
+0
+0
+0.0
+0.0
+*/
+
