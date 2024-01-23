578c575c446e38f5e5d3d1c0ea6d14257e973b4c
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-23 12:38:52 +0530

    Add files via upload

---
+++ Lab4.java
@@ -0,0 +1,32 @@
+package adiiapplication;
+
+public class Lab4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=0/10;
+		System.out.println(a);
+		double d=10.0/00;
+		System.out.println(d);
+		double d1=0.0/10;
+		System.out.println(d1);
+		int b=10%3;
+		System.out.println(b);
+		double d2=1.0/0.0;
+		System.out.println(d2);
+		int x1=10;
+		System.out.println(x1);
+		
+	}
+}
+/*
+O/P:
+0
+Infinity
+0.0
+1
+Infinity
+10
+
+*/
