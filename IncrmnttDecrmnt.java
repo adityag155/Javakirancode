4ad9359cdee5c00f4d06f24d042cc06dd21f2b68
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-23 12:51:00 +0530

    Add files via upload

---
+++ IncrmnttDecrmnt.java
@@ -0,0 +1,27 @@
+package adiiapplication;
+
+public class IncrmnttDecrmnt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=10;
+		System.out.println(x);
+		int a=++x;
+		int b=--x;
+		int c=x++;
+		int d=x--;
+		
+		
+		System.out.println(d);
+	}
+
+}
+/*
+ * O/P:
+10
+11
+10
+10
+11
+*/
