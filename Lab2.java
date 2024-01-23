01fdbd896f98bf85bd4da24d3ee33071639c783f
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-23 11:55:06 +0530

    Add files via upload

--- Lab2.java
+++ Lab2.java
@@ -1,37 +1,25 @@
-class Lab2
-{
+package adiiapplication;
 
-byte b;
-short s;
-int i;
-long l;
-float f;
-double d;
+public class Lab2 {
 
-void show1()
-{
-System.out.println("variable & constant demo");
-System.out.println(b);
-System.out.println(s);
-System.out.println(i);
-System.out.println(l);
-System.out.println(f);
-System.out.println(d);
-}
-public static void main(String args[])
-{
-Lab2 l2=new Lab2();
-l2.show1();
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte b1=10;
+		byte b2=(byte)(b1+2);
+		System.out.println(b2);
+		
+		float f1=9.9f;
+		float f2=(float)(f1+10.0);
+		System.out.println(f2);
+		System.out.println(f1);
+	}
+
 }
 
 /*
 O/P:
-0
-0
-0
-0
-0.0
-0.0
+12
+19.9
+9.9
 */
-
