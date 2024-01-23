7432b53f9e9790fd0a7541510543523e0d44c1d9
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-23 11:42:45 +0530

    Add files via upload

--- Lab1.java
+++ Lab1.java
@@ -1,23 +1,29 @@
-
-class Lab1 {
-    public static void main(String args[])
- {
-
-        String s1 = "Hello guys";
-        String s2 = "123 guys";
-        String s3 = " ";
-        
-	int a = 10;
-        int b = 20;
+package adiiapplication;
 
 
-        System.out.println(-a);
-        System.out.println(b);
-    }
+
+class Lab1
+{
+public static void main(String args[])
+{
+int a=10;
+int $b=20;
+String s1="Hello guys";
+String s2="123 guys";
+String s3=" ";
+
+System.out.println(-a);
+System.out.println($b);
+System.out.println(s1);
+System.out.println(a);
+}
 }
 
 /*
-O/P:
+ * O/P:
+ */
 -10
 20
+Hello guys
+10
 */
