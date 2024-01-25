03a4871e203e00b76e2abda9a8b675c79e586c30
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-25 11:37:17 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,13 @@
+package adiiapplication;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int a = 19;
+	       System.out.println(a << 1);
+	        System.out.println(a << 2);
+	}
+
+}
