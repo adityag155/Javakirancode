45aea7715a04e191e93a89247ebce1106ec2276e
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-25 13:06:13 +0530

    Add files via upload

---
+++ CondtnlPstvorNgtv.java
@@ -0,0 +1,30 @@
+package adiiapplication;
+
+public class CondtnlPstvorNgtv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=20;
+		
+		
+		if(a>0)
+		{
+			System.out.println("The number is Positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("The number is Negative");
+		}
+		else
+		{
+			System.out.println("Invalid Input");
+		}
+	}
+
+}
+
+/*
+O/P:
+The number is Positive
+*/
