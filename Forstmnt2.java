0dc2c90e73572c5aa48afe7872a2e93db8bdf866
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-28 14:18:30 +0530

    Add files via upload

---
+++ Forstmnt2.java
@@ -0,0 +1,36 @@
+package adiiapplication;
+
+public class Forstmnt2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=67;
+		System.out.println("Reverse order");
+		for(int i=n;i>=1;i--)
+		{
+			if(i%5==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
+/*
+O/P:
+Reverse order
+65
+60
+55
+50
+45
+40
+35
+30
+25
+20
+15
+10
+5
+*/
