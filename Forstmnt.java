a3f7dc4975b0d645794191062365fe0e514fdcbd
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-28 14:10:42 +0530

    Add files via upload

---
+++ Forstmnt.java
@@ -0,0 +1,35 @@
+package adiiapplication;
+
+public class Forstmnt {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		int n=67;
+		System.out.println("Forword order");
+		for(int i=1;i<=n;i++)
+		{
+			if(i%5==0)
+				System.out.println(i);
+		}
+	}
+}
+
+/*
+O/P:
+Forword order
+5
+10
+15
+20
+25
+30
+35
+40
+45
+50
+55
+60
+65
+*/
