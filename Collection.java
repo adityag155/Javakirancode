48dc522cc79244808ec6a3f6a816d893cbd4b62a
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-26 19:18:30 +0530

    Add files via upload

--- Collection.java
+++ Collection.java
@@ -13,11 +13,30 @@ System.out.println(Name);
 		
 Name.add("Ajith");
 System.out.println(Name);
+
+Name.add(1,"Saurabh");
+System.out.println(Name);
+
+Name.remove(4);
+System.out.println(Name);
+
+Name.set(0,"Kedar");			//Set method can be used to replace value
+System.out.println(Name);
+
+System.out.println(Name.get(2));	//get method is used to print specific value
+
+Name.clear();
+System.out.println(Name);
 }
 }
 
 /*
  * O/P:
- * [Adi, Anu, Ani, Omii]
-[Adi, Anu, Ani, Omii, Ajith]
+ * [Adi, Anu, Ani, Omi]
+[Adi, Anu, Ani, Omi, Ajith]
+[Adi, Saurabh, Anu, Ani, Omi, Ajith]
+[Adi, Saurabh, Anu, Ani, Ajith]
+[Kedar, Saurabh, Anu, Ani, Ajith]
+Anu
+[]
 */
