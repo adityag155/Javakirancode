5116b021a7582c5add17efaa53a477f4d04b9610
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-27 23:03:12 +0530

    Add files via upload

---
+++ Serlzn.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class Box implements Serializable
+{
+double width,height,length;
+Box(double w, double h, double l)
+{
+width=w;
+height=h;
+length=l;
+}
+}
+class Serlzn
+{
+public static void main(String args[])throws IOException
+{
+Box b1=new Box(20,30,40);
+
+FileOutputStream fos=new FileOutputStream("India.txt");
+ObjectOutputStream oos=new ObjectOutputStream(fos);
+oos.writeObject(b1);
+}
+}
+
+//In the India.txt file width height length will be printed
+//fos can be used to write data India.txt file
+//oos can be used used to write content in fos means in txt file
