5cd635ceed78dfa3c6711e10f5f3fa635cadcb91
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-29 11:36:01 +0530

    Add files via upload

---
+++ StaticInstncMeth.java
@@ -0,0 +1,30 @@
+public class StaticInstncMeth
+{
+int a=10;
+static int b=20;
+void jbk()
+{
+System.out.println("Hello JBK");
+System.out.println(a);
+System.out.println(b);
+}
+static void adi()
+{
+System.out.println("Hello jbk");
+System.out.println(b);
+}
+public static void main(String args[])
+{
+//StaticInstance si = new StaticInstance();
+StaticInstncMeth si = new StaticInstncMeth();
+
+System.out.println(si.a);
+System.out.println(si.b);
+//si.adi();     			//if we call adi method in main method thts why not printed
+}
+}
+/*
+O/P:
+10
+20
+*/
