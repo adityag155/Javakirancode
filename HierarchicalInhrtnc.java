6d4a6486ca9e73d4afc55e596018dd91ba4544b7
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-03-07 13:23:29 +0530

    Add files via upload

---
+++ HierarchicalInhrtnc.java
@@ -0,0 +1,56 @@
+class A
+{
+public void methodA()
+{
+System.out.println("Method of class A");
+}
+}
+class B extends A
+{
+void methodB()
+{
+System.out.println("Method of class B");
+}
+}
+
+class C extends A
+{
+void methodC()
+{
+System.out.println("Method of class C");
+}
+}
+
+class D extends A
+{
+void methodD()
+{
+System.out.println("Method of class D");
+}
+}
+
+
+class HierarchicalInhrtnc
+{
+void methodB()
+{
+System.out.println("method of class B");
+}
+public static void main(String args[])
+{
+B obj1=new B();
+C obj2=new C();
+D obj3=new D();
+
+obj1.methodA();
+obj2.methodA();
+obj3.methodA();
+}
+}
+
+/*
+O/P:
+Method of class A
+Method of class A
+Method of class A
+*/
