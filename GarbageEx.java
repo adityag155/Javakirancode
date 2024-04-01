0ee1a3378e2916f8590967ac3b69d91fdba1a32e
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-04-01 12:43:30 +0530

    Add files via upload

---
+++ GarbageEx.java
@@ -0,0 +1,22 @@
+class GarbageEx
+{
+public void finalize()
+{
+System.out.println("Garbage Collection");
+}
+public static void main(String args[])
+{
+GarbageEx g1=new GarbageEx();
+GarbageEx g2=new GarbageEx();
+
+g1=null;
+g2=null;
+g1.finalize();
+System.gc();
+}
+}
+
+/*
+O/P:
+Garbage Collection
+*/
