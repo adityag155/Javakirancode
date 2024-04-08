e123dbb7fae3f4ab7600b484091413679209b533
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-04-08 11:46:30 +0530

    Add files via upload

---
+++ FileWrite.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class FileWrite
+{
+public static void main(String args[])
+{
+try
+{
+FileWriter f=new FileWriter("C:\\Aditya\\JavaKiran\\Adu2.txt");
+try
+{
+f.write("Java Programming is the best language");
+}
+finally
+{
+f.close();
+}
+System.out.println("Successfully write data in file");
+}
+catch(IOException i)
+{
+System.out.println(i);
+}
+}
+}
+
+//After excection the file name created in given location and also printed that message.
