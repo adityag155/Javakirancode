ddd60b5776fa808a5ff018c57b525942400e20b6
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-04-09 21:34:10 +0530

    Update Bitwise.java

--- Bitwise.java
+++ Bitwise.java
@@ -4,7 +4,6 @@ public class Bitwise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		 int a = 19;
 	       System.out.println(a << 1);
 	        System.out.println(a << 2);
